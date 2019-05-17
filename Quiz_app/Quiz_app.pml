9b37fec222402e5e80d931159c61061f8000d025
gordo851 <gr2@hw.ac.uk>
2019-05-17 10:20:10 +0100

    project made

---
+++ Quiz_app/Quiz_app.pml
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Quiz_app" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="choice_sentences" src="behavior_1/Aldebaran/choice_sentences.xml" />
+        <File name="choice_sentences_light" src="behavior_1/Aldebaran/choice_sentences_light.xml" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ Quiz_app/translations/translation_en_US.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>ok, thanks anyway</source>
+            <comment>Text</comment>
+            <translation type="unfinished">ok, thanks anyway</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sorry I didn't understand that, try answering with yes or no</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sorry I didn't understand that, try answering with yes or no</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Hurray, let's play then</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello</translation>
+        </message>
+    </context>
+</TS>
